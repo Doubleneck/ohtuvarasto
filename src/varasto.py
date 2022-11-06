7850580ee4d5166b644739219a624987cde6bf17
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-11-06 18:58:47 +0200

    vko2 teht 3

--- src/varasto.py
+++ src/varasto.py
@@ -1,15 +1,11 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            #testimuutos 4
-            self.tilavuus = 0.0
-        
+        self.tilavuus = max(tilavuus,0.0)
+
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
+
         elif alku_saldo <= tilavuus:
             # mahtuu
             self.saldo = alku_saldo
@@ -17,7 +13,8 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -32,7 +29,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -41,6 +38,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
